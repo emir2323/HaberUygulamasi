554539d64678901604361bb421263cfb6890e996
Emir Yıldırım <emir76931@gmail.com>
2024-05-17 08:35:16 +0300

    HeadLine oluşturulması

---
+++ my-app-master/App/Components/Home/HeadlineList.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { View, Text } from "react-native";
+
+function HeadlineList() {
+  return (
+    <View>
+      <Text>HeadLine List</Text>
+    </View>
+  );
+}
+
+export default HeadlineList;
